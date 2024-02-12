1c14cba3cfa5fc99c11102ac39174fb462f03919
99Rafick <rafraf9727@gmail.com>
2024-02-12 18:08:08 +0100

    Typo: Display Hero Banner

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 
 import Header from "@/views/partials/Header.vue";
+import HeroBanner from "@/views/partials/HeroBanner.vue";
 </script>
 
 <template>
     <Header />
     <main>
-
+        <HeroBanner />
     </main>
 </template>
