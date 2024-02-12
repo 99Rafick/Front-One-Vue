7840a2822e1a884dfe4a01bc62b06e5b2e236794
99Rafick <rafraf9727@gmail.com>
2024-02-12 18:07:51 +0100

    Typo: Implement Hero banner

---
+++ src/views/partials/HeroBanner.vue
@@ -0,0 +1,27 @@
+<script setup>
+import SecondaryButton from "@/components/SecondaryButton.vue";
+import IconRightButton from "@/components/IconRightButton.vue";
+</script>
+<template>
+    <section id="hero-banner">
+        <div class="hero-banner-left lg:p-16 relative flex items-end" style="background-image: url('https://images.pexels.com/photos/2569997/pexels-photo-2569997.jpeg'); background-size: cover">
+            <div class="flex items-center justify-between">
+              <div>
+                <span class="text-white text-extrasmall text-gray-300 uppercase font-light">SOFTWARE DEVELOPPEMENT SERVICE</span>
+                <h2 class="text-white mt-3 w-8/12 text-big">We are website & app  development agency</h2>
+              </div>
+              <SecondaryButton class="flex-none" name="Get in touch" link="#" />
+            </div>
+        </div>
+        <div class="hero-banner-right overflow-hidden">
+            <img class="h-96 rounded-3xl object-cover w-full" src="https://images.pexels.com/photos/5240140/pexels-photo-5240140.jpeg" alt="Image">
+            <div class="flex mt-5 items-center justify-between">
+              <div class="">
+                <span class="text-extrasmall uppercase text-gray-500 font-light">OUR LATEST WORK</span>
+                <h2 class="w-8/12 text-smallbig">FastBank</h2>
+              </div>
+              <IconRightButton class="flex-none" link="#" />
+            </div>
+        </div>
+    </section>
+</template>
