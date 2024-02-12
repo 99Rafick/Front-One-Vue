be5bd0c5c0d49dfbb2e9a457b3d8cf8e51259e96
99Rafick <rafraf9727@gmail.com>
2024-02-12 18:06:57 +0100

    Feat: create Icon Right Button

---
+++ src/components/IconRightButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <a :href="link" class="w-12 h-12 flex items-center justify-center flex-none rounded-full border">
+    <ion-icon class="text-3xl" name="arrow-forward-outline"></ion-icon>
+  </a>
+</template>
