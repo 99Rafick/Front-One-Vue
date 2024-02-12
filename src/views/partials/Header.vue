d430cbf006bd45e294a12db026fd733a0fa37438
99Rafick <rafraf9727@gmail.com>
2024-02-12 17:07:31 +0100

    Typo: display header

--- src/views/partials/Header.vue
+++ src/views/partials/Header.vue
@@ -1,3 +1,21 @@
+<script setup>
+   import PrimaryButton from "@/components/PrimaryButton.vue";
+</script>
 <template>
+  <header>
+      <div>
+          <h1 id="title">Reemax</h1>
+          <nav>
+            <ul class="flex items-center gap-10">
+              <li><a href="#">Accueil</a></li>
+              <li><a href="#">Service</a></li>
+              <li><a href="#">Avis</a></li>
+              <li><a href="#">Plan</a></li>
+              <li><a href="#">Faq</a></li>
+            </ul>
+          </nav>
+      </div>
 
+      <PrimaryButton name="Project estimation" link="#" />
+  </header>
 </template>
