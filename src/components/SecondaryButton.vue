8ab992b537b655e7b7a7078a0254462b5b7ec0a8
99Rafick <rafraf9727@gmail.com>
2024-02-12 18:07:19 +0100

    Feat: create secondary Button

---
+++ src/components/SecondaryButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  link: String,
+  name: String,
+})
+
+</script>
+
+
+<template>
+  <a :href="link" class="button-size bg-white text-blue-500">{{ name }}</a>
+</template>
