a3c04aea25d63cc8aa150213af5b8a3909964d60
99Rafick <rafraf9727@gmail.com>
2024-02-12 17:06:44 +0100

    Feat: create primary Button component

---
+++ src/components/PrimaryButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+ defineProps({
+    link: String,
+    name: String,
+ })
+
+</script>
+
+
+<template>
+  <a :href="link" class="button-size bg-blue-500/10 text-blue-500">{{ name }}</a>
+</template>
