96d6fbad517495f245da45d12429c47e94dcf1c2
99Rafick <rafraf9727@gmail.com>
2024-02-13 11:12:36 +0100

    Typo: display partial

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 
 import Header from "@/views/partials/Header.vue";
 import HeroBanner from "@/views/partials/HeroBanner.vue";
+import Sponsors from "@/views/partials/Sponsors.vue";
 </script>
 
 <template>
     <Header />
     <main>
         <HeroBanner />
+        <Sponsors />
     </main>
 </template>
