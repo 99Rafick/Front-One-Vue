43d8936953a9cc1908b9a1db63541b01bce2dbed
99Rafick <rafraf9727@gmail.com>
2024-02-13 11:12:08 +0100

    Typo: create Sponsors partial

---
+++ src/views/partials/Sponsors.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+<template>
+    <section id="sponsors" class="relative mt-10 w-full min-h-28">
+        <div class="w-[105%] absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex items-center justify-between">
+          <img src="/img/asana-500.png" alt="asana" class="h-28 object-cover">
+          <img src="/img/asana-500.png" alt="asana" class="h-28 object-cover">
+          <img src="/img/asana-500.png" alt="asana" class="h-28 object-cover">
+          <img src="/img/asana-500.png" alt="asana" class="h-28 object-cover">
+          <img src="/img/asana-500.png" alt="asana" class="h-28 object-cover">
+          <img src="/img/asana-500.png" alt="asana" class="h-28 object-cover">
+          <img src="/img/asana-500.png" alt="asana" class="h-28 object-cover">
+        </div>
+    </section>
+</template>
