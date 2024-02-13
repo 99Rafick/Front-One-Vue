6ed05c5295d39ac68bb45b02b70dfe3d85961a71
99Rafick <rafraf9727@gmail.com>
2024-02-13 16:11:26 +0100

    Typo: set right border

--- src/components/IconRightButton.vue
+++ src/components/IconRightButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :href="link" class="w-12 h-12 flex items-center justify-center flex-none rounded-full border">
+  <a :href="link" class="w-12 h-12 flex items-center justify-center flex-none rounded-full border border-gray-200">
     <ion-icon class="text-3xl" name="arrow-forward-outline"></ion-icon>
   </a>
 </template>
