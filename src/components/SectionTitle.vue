510f0f1e50fbf966f0fccc15479b6911269b0336
99Rafick <rafraf9727@gmail.com>
2024-02-13 16:12:27 +0100

    Feat: create section title component

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import {computed} from "vue";
+
+    const props = defineProps({
+        subTitle: String,
+        title: String,
+        colorWhite: {
+            type: Boolean,
+            default: false,
+        }
+    })
+
+    const getTextClasses = computed(() => {
+      return {
+        'text-extrasmall': true,
+        'uppercase': true,
+        'font-light': true,
+        'text-gray-300': props.colorWhite,
+        'text-gray-400': !props.colorWhite,
+      };
+    });
+
+    const getTitleClasses = computed(() => {
+      return {
+        'mt-5': true,
+        'text-big': true,
+        'text-white': props.colorWhite,
+      };
+    });
+</script>
+<template>
+  <div>
+    <span :class="getTextClasses">{{ subTitle }}</span>
+    <h2 :class="getTitleClasses">{{ title }}</h2>
+  </div>
+</template>
