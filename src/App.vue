b8a3196a264bbc84c97f9d33d8ff4e4b48463756
99Rafick <rafraf9727@gmail.com>
2024-02-13 17:08:47 +0100

    Typo: display service

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import Header from "@/views/partials/Header.vue";
 import HeroBanner from "@/views/partials/HeroBanner.vue";
 import Sponsors from "@/views/partials/Sponsors.vue";
 import Works from "@/views/partials/Works.vue";
+import Services from "@/views/partials/Services.vue";
 </script>
 
 <template>
@@ -12,5 +13,6 @@ import Works from "@/views/partials/Works.vue";
         <HeroBanner />
         <Sponsors />
         <Works />
+        <Services />
     </main>
 </template>
