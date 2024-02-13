06ad99d36676c18402de45eb9401a6ec7dc94aad
99Rafick <rafraf9727@gmail.com>
2024-02-13 17:06:43 +0100

    Feat: create info button component

---
+++ src/components/InfoButton.vue
@@ -0,0 +1,10 @@
+<script setup>
+defineProps({
+  link: String,
+  name: String,
+})
+
+</script>
+<template>
+  <a :href="link" class="button-size bg-white/10 text-white">{{ name }}</a>
+</template>
