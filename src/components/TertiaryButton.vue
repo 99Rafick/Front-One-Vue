7cd5ceae779e18466d06124167632d07541c0370
99Rafick <rafraf9727@gmail.com>
2024-02-15 16:25:08 +0100

    Feat: create button component

---
+++ src/components/TertiaryButton.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  link: String,
+  name: String,
+})
+
+</script>
+
+
+<template>
+  <a :href="link" class="button-size bg-blue-500 text-white">{{ name }}</a>
+</template>
